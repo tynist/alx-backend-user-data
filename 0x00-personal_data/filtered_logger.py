32ee9201dc70197f3826380df4a0e3c59644444d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-25 10:34:55 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 """
-Regex-ing
+Personal data
 """
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """Initialize RedactingFormatter object."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record, redacting specified fields."""
+        log_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log_message, self.SEPARATOR)
 
 
 def filter_datum(fields, redaction, message, separator):
