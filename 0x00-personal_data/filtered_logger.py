8fa0bb78fe1c2d0899cd44de788dedc529fa1209
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-25 11:02:58 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,47 +1,109 @@
 #!/usr/bin/env python3
 """
-Personal data
+filter_datum
 """
+from typing import List
 import re
 import logging
+import mysql.connector
+import os
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    '''Redacting Formatter class.
+    '''
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
-        """Initialize RedactingFormatter object."""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields: List[str]):
+        '''
+        Constructor
+        '''
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format the log record, redacting specified fields."""
-        log_message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, log_message, self.SEPARATOR)
+        '''
+        Filters values in incoming log records using `filter_datum`
+        '''
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    Obfuscates the specified fields in the log message.
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    '''
+        returns the log message obfuscated:
+            - fields: a list of strings representing all fields to obfuscate
+            - redaction: a string representing by what the field will
+              be obfuscated
+            - message: a string representing the log line
+            - separator: a string representing by which character is
+              separating all fields in the log line (message)
+    '''
+    for field in fields:
+        message = re.sub(rf"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
 
-    Arguments:
-    - fields: list of strings representing all fields to obfuscate.
-    - redaction: string representing by what the field will be obfuscated.
-    - message: string represents the log line.
-    - separator: string representing by which character separats all fields
 
-    Returns:
-    - The obfuscated log message.
+def get_logger() -> logging.Logger:
 
-    """
-    # Create an expression pattern to match the fields with the seperator
-    pattern = r"(?<={}=)[^{}]+".format(separator, separator)
+    ''' Creates Logger
+    Logger: user_data
+    Level: INFO
+    Doesn't Propagate msgs to other loggers
+    '''
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
 
-    # substitution using the regular expression pattern & redaction value
-    obfuscated_message = re.sub(pattern, redaction, message)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
 
-    # Return the obfuscated log message
-    return obfuscated_message
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+
+    '''
+    returns a MySQL Database connector
+    '''
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    connector = mysql.connector.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=database)
+
+    return connector
+
+
+def main():
+    '''
+    a main function that takes no arguments and
+    returns nothing.
+    '''
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    data = cursor.fetchall()
+    for row in data:
+        for coloumn in row:
+            print(coloumn)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
