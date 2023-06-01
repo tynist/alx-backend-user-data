dc0725d0feb095a4d376ea305dab5017b53040da
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-01 04:05:49 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,8 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask_cors import (CORS, cross_origin)
 import os
 
@@ -13,14 +15,32 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+if getenv('AUTH_TYPE') == 'auth':
+    auth = Auth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    auth = BasicAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def not_found(error) -> str:
+    """Unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def not_found(error) -> str:
+    """Forbidden handler"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+# Your other routes and endpoints
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
