8de27f5a2e9e3d55a603056662b66953eb92d7d9
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-02 00:39:52 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,7 +2,6 @@
 """ Module of Users views
 """
 from os import getenv
-
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
@@ -10,23 +9,27 @@ from models.user import User
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    """ POST /auth_session/login
-    Return:
-      - User object JSON represented
-      - Sets a cookie for the user
+    """
+    Log in a user and return their user object JSON representation.
+    Returns:
+        - JSON representation of the User object.
+        - Sets a cookie for the user.
     """
     email = request.form.get('email')
     password = request.form.get('password')
+    
     if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
     if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
+    
     user = User.search({'email': email})
     if len(user) == 0:
         return jsonify({"error": "no user found for this email"}), 404
     user = user[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
+    
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     cookie_name = getenv('SESSION_NAME')
@@ -39,10 +42,11 @@ def login() -> str:
                  methods=['DELETE'],
                  strict_slashes=False)
 def logout() -> str:
-    """ DELETE /auth_session/logout
-    Return:
-      - Empty JSON
-      - Deletes the user session / logout
+    """
+    Log out a user and delete their session.
+    Returns:
+        - Empty JSON response.
+        - Deletes the user session / logs out the user.
     """
     from api.v1.app import auth
     if auth.destroy_session(request) is False:
