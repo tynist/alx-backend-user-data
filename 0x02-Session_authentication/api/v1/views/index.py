a0859c045f239477a579c9ea9a8c545b57859f0e
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-01 18:02:20 +0100

    Update index.py

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -7,10 +7,10 @@ from api.v1.views import app_views
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def authorized() -> str:
+def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
-        - raise a 401 error
+      - Raise 401 error
     """
     abort(401, description="Unauthorized")
 
