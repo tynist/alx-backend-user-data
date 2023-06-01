141ae58ac9f09bbedcf35eed74bd1d3bd0bf5307
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-01 12:01:40 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,9 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
+from flask import Flask, abort, jsonify, request
 from flask_cors import (CORS, cross_origin)
 import os
 
@@ -14,48 +12,57 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
+
 if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
 elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def not_found(error) -> str:
-    """Unauthorized handler"""
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def not_found(error) -> str:
-    """Forbidden handler"""
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
-@app.before_request
-def before_request():
+def before_request() -> None:
     """
-    handler before_request
+    Filters request
     """
-    authorized_list = ['/api/v1/status/',
-                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        pass
+    else:
+        if auth.require_auth(request.path, [
+                             '/api/v1/status/',
+                             '/api/v1/unauthorized/',
+                             '/api/v1/forbidden/'
+                             ]):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
-    if auth and auth.require_auth(request.path, authorized_list):
-        if not auth.authorization_header(request):
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
 
+app.before_request(before_request)
 
-# Your other routes and endpoints
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
