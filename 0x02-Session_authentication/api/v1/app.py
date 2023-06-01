f6836688f7e47c8c2e289a456a7396ad6ff35384
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-01 17:48:11 +0100

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,6 +24,12 @@ elif getenv('AUTH_TYPE') == 'basic_auth':
 elif getenv("AUTH_TYPE") == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif getenv("AUTH_TYPE") == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv("AUTH_TYPE") == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.before_request
@@ -41,10 +47,9 @@ def before_request():
             '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and\
+                auth.session_cookie(request) is None:
                 abort(401, description="Unauthorized")
-            if auth.session_cookie(request) is None:
-                abort(404, description="Not found")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
 
