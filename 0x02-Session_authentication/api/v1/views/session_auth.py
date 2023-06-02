c27291081e73b5f4e4b7b583205537b983e2c9cd
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-02 07:29:22 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -17,19 +17,19 @@ def login() -> str:
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    
+
     if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
     if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-    
+
     user = User.search({'email': email})
     if len(user) == 0:
         return jsonify({"error": "no user found for this email"}), 404
     user = user[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    
+
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     cookie_name = getenv('SESSION_NAME')
